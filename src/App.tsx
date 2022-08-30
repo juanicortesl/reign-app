1c551f75c7670787151939f6be58c3217938caf2
juanicortesl <jicortes6@uc.cl>
2022-08-30 16:45:16 -0400

    feat(app/Home): create app header

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="App-Header">
+        <h1>Hacker News</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
