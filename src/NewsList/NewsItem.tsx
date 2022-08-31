e4f4dff28d7057492f7e744d44534c95714efbe5
juanicortesl <jicortes6@uc.cl>
2022-08-30 20:47:05 -0400

    feat(newsList): add option for selecting news type

--- src/NewsList/NewsItem.tsx
+++ src/NewsList/NewsItem.tsx
@@ -5,6 +5,12 @@ interface Props {
 }
 
 function NewsItem({ data }: Props) {
+  const getHoursSince = (date: Date) => {
+    const now = new Date().getTime();
+    const hourInMilliseconds = 1000 * 60 * 60;
+    return Math.round((now - date.getTime()) / hourInMilliseconds);
+  };
+
   return (
     <div className="News-Item">
       <div className="News-Item-Col-1">
@@ -17,10 +23,11 @@ function NewsItem({ data }: Props) {
           </div>
 
           <p>
-            {data.hours} hours ago by {data.autor}
+            {getHoursSince(new Date(data.created_at))} hours ago by{" "}
+            {data.author}
           </p>
         </div>
-        <h2 className="News-Title">{data.title}</h2>
+        <h2 className="News-Title">{data.story_title || data.title}</h2>
       </div>
       <div className="News-Item-Col-2">
         <img
--- src/NewsList/NewsList.tsx
+++ src/NewsList/NewsList.tsx
@@ -1,52 +1,43 @@
 import React from "react";
+import {
+  convertCompilerOptionsFromJson,
+  textChangeRangeNewSpan,
+} from "typescript";
 import "../App.css";
 import NewsItem from "./NewsItem";
 
 function NewsList() {
-  const [newsData, setNewsData] = React.useState([
-    {
-      autor: "author",
-      hours: 3,
-      title: "Event-driven state management in React using Storeon",
-    },
-    {
-      autor: "author",
-      hours: 3,
-      title: "Event-driven state management in React using Storeon",
-    },
-    {
-      autor: "author",
-      hours: 3,
-      title: "Event-driven state management in React using Storeon",
-    },
-    {
-      autor: "author",
-      hours: 3,
-      title: "Event-driven state management in React using Storeon",
-    },
-    {
-      autor: "author",
-      hours: 3,
-      title: "Event-driven state management in React using Storeon",
-    },
-    {
-      autor: "author",
-      hours: 3,
-      title: "Event-driven state management in React using Storeon",
-    },
-    {
-      autor: "author",
-      hours: 3,
-      title: "Event-driven state management in React using Storeon",
-    },
-    {
-      autor: "author",
-      hours: 3,
-      title: "Event-driven state management in React using Storeon",
-    },
-  ]);
+  const [newsData, setNewsData] = React.useState([]);
+  const [selectedNews, setSelectedNews] = React.useState("");
+  const getNewsData = async () => {
+    console.log("getting news");
+    const res = await fetch(
+      `https://hn.algolia.com/api/v1/search_by_date?query=${selectedNews}&page=0`
+    );
+    const json = await res.json();
+    setNewsData(json.hits);
+  };
+  React.useEffect(() => {
+    getNewsData();
+  }, [selectedNews]);
+  const changeSelectedNews = (event: any) => {
+    console.log("changing news");
+    setSelectedNews(event.target.value);
+  };
   return (
     <div className="News-List">
+      <div className="custom-select">
+        <select
+          placeholder="Select your news"
+          value={selectedNews}
+          onChange={changeSelectedNews}
+        >
+          <option value="angular">Angular</option>
+          <option value="reactjs">React</option>
+          <option value="vuejs">VueJS</option>
+        </select>
+      </div>
+
       <div className="News-List-Wrapper">
         {newsData.map((news, index) => {
           return <NewsItem key={index} data={news}></NewsItem>;
